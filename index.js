ae4e914aad74c1686a2c6a1fa20654b21384f69a
wkdewey@gmail.com <wkdewey@gmail.com>
2018-05-27 22:36:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function viewCart() {
       if (item < (cart.length - 1)) {
         statement += ", ";
       }
-      if (item === (cart.length - 2)) {
+      if (item === 0) {
           statement += "and "
       }
     }
