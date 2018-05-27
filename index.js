9ca6f8875b297977f4c075458e96a7b7eea25b5d
wkdewey@gmail.com <wkdewey@gmail.com>
2018-05-27 23:40:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (not cardNumber) {
+  if (cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   }
 }
